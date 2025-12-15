cc6f6e5fbc873036f09eb2a8d4cf6b54b26cbb90
Niladri <niladrisengit12@gmail.com>
2025-12-15 12:19:47 +0530

    feat: initialize hms-api-gateway and hms-discovery-service with basic configurations and dependencies

---
+++ backend/hms-api-gateway/src/test/java/com/hms/hms_api_gateway/HmsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.hms_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/hms-discovery-service/src/test/java/com/hms/hms_discovery_service/HmsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.hms_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
